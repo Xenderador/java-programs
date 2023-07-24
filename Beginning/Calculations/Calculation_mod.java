c697fc117767416edf5f55fc72fceb934d47ba61
Your Name <22kartiksanil@gmail.com>
2023-07-24 11:16:47 +0530

    Create Calculation_mod.java

---
+++ Beginning/Calculations/Calculation_mod.java
@@ -0,0 +1,12 @@
+package Beginning.Calculations;
+
+public class Calculation_mod {
+    public static void main(String[] args){
+        int x=5;
+        int y=10;
+        System.out.println("The sum is :"+(x+y));
+        System.out.println("The difference is :"+(x-y));
+        System.out.println("The product is :"+(x*y));
+        System.out.println("The division is :"+(x/y));
+        }
+}
