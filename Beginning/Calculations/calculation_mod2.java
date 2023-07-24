82e8f2632a8566ca7b30503c3b4580f2cc928a37
Your Name <22kartiksanil@gmail.com>
2023-07-24 14:50:36 +0530

    Create calculation_mod2.java

---
+++ Beginning/Calculations/calculation_mod2.java
@@ -0,0 +1,11 @@
+package Beginning.Calculations;
+
+public class calculation_mod2 {
+    static int myMethod(int x, int y){     
+       return x + y;
+    }
+    public static void main(String[] args){
+        int z= myMethod(5,3);
+        System.out.println(z);
+    }
+}
