032d9037ee093a0da0a1b87ae5b75fa123a1307e
Your Name <22kartiksanil@gmail.com>
2023-07-24 12:15:28 +0530

    String Parameter and Arguments

---
+++ Beginning/Parameters_and_Arguments/p_a.java
@@ -0,0 +1,12 @@
+package Beginning.Parameters_and_Arguments;
+
+public class p_a {
+    static void myMethod(String fname){
+       System.out.println(fname + " Seager");
+    }
+    public static void main(String args[]){
+       myMethod("Marcus");
+       myMethod("Kereus");
+       myMethod("Lily");
+    }
+}
