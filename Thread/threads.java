e8737d27004288f813e00961e29ccd4facf5ec91
Your Name <22kartiksanil@gmail.com>
2023-08-05 16:47:43 +0530

    Create threads.java

---
+++ Thread/threads.java
@@ -0,0 +1,32 @@
+/* Write a java program that implements Thread class methods */
+
+package Thread;
+
+class threads extends Thread{
+    public void run(){
+        for(int i = 1; i <= 5; i++){
+            try{
+                System.out.println("running thread name is: " + Thread.currentThread().getName());
+                System.out.println("running thread priority is:" + Thread.currentThread().getPriority());
+            }catch (Exception e){
+                System.out.println(e);
+            }
+            System.out.println(i);
+        }
+    }
+    public static void main(String args[]) {
+        threads t0 = new threads();
+        threads t1 = new threads();
+        threads t2 = new threads();
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t0.run();
+        try {
+            t0.join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        t1.run();
+        t2.run();
+    }
+}
